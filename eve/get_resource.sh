71c949afb1f86e7d42e186519e8b93b4c32a9075
Claude Ebaneck <claude.ebaneck@scality.com>
2020-03-30 05:37:21 +0200

    test

--- eve/get_resource.sh
+++ eve/get_resource.sh
@@ -2,15 +2,15 @@
 
 ATTEMPTS=0
 declare -a GET_COLUMN_VALUE=(
-    sudo kubectl --kubeconfig "${KUBECONFIG}" get "${RESOURCE_TYPE}"
+    kubectl --kubeconfig "${KUBECONFIG}" get "${RESOURCE_TYPE}"
     "${RESOURCE_NAME}" -n "${NAMESPACE}" -o "custom-columns=${RESOURCE_COLUMN}"
 )
 CURRENT_VALUE=$("${GET_COLUMN_VALUE[@]}")
 until [ "${CURRENT_VALUE}" -eq "${RESOURCE_VALUE}" ]; do
     (( ATTEMPTS++ ))
     if [ "${ATTEMPTS}" -eq "${RETRIES}" ]; then
-    echo "${RESOURCE_COLUMN} still not equal to ${RESOURCE_VALUE} after ${ATTEMPTS} attempts" >&2
-    exit 1
+        echo "${RESOURCE_COLUMN} still not equal to ${RESOURCE_VALUE} after ${ATTEMPTS} attempts" >&2
+        exit 1
     fi
     sleep "${SLEEP}"
     CURRENT_VALUE=$("${GET_COLUMN_VALUE[@]}")
